6bcc81d498b2b8248e2495eab8dbd1e1ed5c6e00
dgomez40 <163152659+dgomez40@users.noreply.github.com>
2025-08-01 00:00:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     directories = ['data/part2_plots', 'data/part3_plots', 'data/part4_plots', 'data/part5_plots']
     part1.create_directories(directories)
     
-    pred_universe, arrest_events, charge_counts, charge_counts_by_offense = part1.extract_transform()
+    pred_universe, arrest_events, charge_counts, charge_counts_by_offense, felony_charge, pred_universe_fel_merge = part1.extract_transform()
     
     ##  PART 2: PLOT EXAMPLES  ##
     # Apply plot theme
@@ -30,25 +30,35 @@ def main():
 
     ##  PART 3: BAR PLOTS AND HISTOGRAMS  ##
     # 1
-
+    part3.fta_barplot(pred_universe)
     # 2
-
+    part3.fta_barplot_sex(pred_universe)
     # 3
-
+    part3.hist_age_arrest(pred_universe)
     # 4
-
+    part3.hist_age_arrest_with_bins(pred_universe)
+    
+    
     ##  PART 4: CATEGORICAL PLOTS  ##
     # 1
-    
+    part4.catplot_felony_rearrest_pred(pred_universe_fel_merge)
     # 2
+    part4.catplot_nonfelony_rearrest_pred(pred_universe_fel_merge)
+   
+    part4.observations()
 
     # 3
+    part4.catplot_felony_actual_outcome(pred_universe_fel_merge)
+    part4.observations_2()
+
 
     ##  PART 5: SCATTERPLOTS  ##
     # 1
-    
+    part5.scatter_prediction_felony_vs_nonfelony(pred_universe)
+    part5.oberservation()
     # 2
-
+    part5.scatter_prediction_felony_vs_nonfelony(pred_universe)
+    part5.observation2()
 
 if __name__ == "__main__":
     main()
